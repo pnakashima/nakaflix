30bc2b3036aa984ea6ea98122097eee320c51ef8
Paulo Nakashima <p.nakashima@hotmail.com>
2022-01-31 17:20:10 -0300

    clone do netflix utilizando python e django

--- nakaflix/settings.py
+++ nakaflix/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-8=3!2ev&jxqxemdirg*ie#6fg0v7=(l&o$*#!429d&n_3t017^'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', 'naka-flix.herokuapp.com']
 
 
 # Application definition
